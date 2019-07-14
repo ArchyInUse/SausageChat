957e28e5f0b7ce624064053b4311c3ce72d303f4
ArchyInUse <archyinuse@gmail.com>
2019-07-15 02:52:10 +0300

    Removed unnecessary using statements

--- SausageChat/Networking/Server.cs
+++ SausageChat/Networking/Server.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using SausageChat.Messaging;
-using SausageChat.Networking;
 using SausageChat.Helpers;
 using System.Collections.ObjectModel;
 
--- SausageChat/Networking/ServerCommandResult.cs
+++ SausageChat/Networking/ServerCommandResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SausageChat.Networking
+﻿namespace SausageChat.Networking
 {
     /// <summary>
     /// result from a server command (kick, mute, ban etc) on a user
