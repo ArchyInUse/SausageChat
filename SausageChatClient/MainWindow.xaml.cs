36be1297552e55e8c7a9025326b0cfb097e6d401
mantie <mantiereid2@gmail.com>
2019-07-19 02:50:11 -0400

    ComboBox for ip will now start with Disco as the default one.  Added some code to help create placeholder text later on.

--- SausageChatClient/MainWindow.xaml.cs
+++ SausageChatClient/MainWindow.xaml.cs
@@ -31,8 +31,35 @@ namespace SausageChatClient
             SausageClient.Mw = this;
             SausageClient.Vm = Vm;
             this.DataContext = Vm;
-      Select_IP_Combo_box.SelectedIndex = ;
-        }
+    }
+
+
+
+    public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      // Always test MultiValueConverter inputs for non-null
+      // (to avoid crash bugs for views in the designer)
+      if (values[0] is bool && values[1] is bool)
+      {
+        bool hasText = !(bool)values[0];
+        bool hasFocus = (bool)values[1];
+
+        if (hasFocus || hasText)
+          return Visibility.Collapsed;
+      }
+
+      return Visibility.Visible;
+    }
+
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+
+
+
+
 
     // public void Log(IMessage message)
     // {
@@ -40,6 +67,36 @@ namespace SausageChatClient
     //}
 
 
+
+
+
+    //  Disco's IP  =  index 0
+    // Monte's IP  = This is index 1
+
+    public string Which_IP_is_selected(string option)
+    {
+
+      if(Select_IP_Combo_box.SelectedIndex == 0)
+      {
+        //user has selected Disco IP
+         option = "disco's IP";
+        
+
+      }
+
+      if(Select_IP_Combo_box.SelectedIndex == 1)
+      {
+        //user has selected  Montes IP
+        option = "Montes IP";
+
+
+
+      }
+
+      
+
+      return option;
+    }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
