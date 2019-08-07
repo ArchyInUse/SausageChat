01602a193a6209a284a16c6eabb8ea73fb9b1c93
mantie <mantiereid2@gmail.com>
2019-08-06 23:41:53 -0400

    Changed colors of friends list to discord colors.  Need to work on the freinds list and the list of users colors.

--- SausageChatClient/Client_view_window.xaml.cs
+++ SausageChatClient/Client_view_window.xaml.cs
@@ -21,14 +21,10 @@ using System.Windows.Shapes;
 
 namespace SausageChatClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+   
     public partial class MainWindow
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindow"/> class.
-        /// </summary>
+        
         public MainWindow()
         {
             SausageUserList.UiCtx = SynchronizationContext.Current;
@@ -60,10 +56,7 @@ namespace SausageChatClient
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The Which_IP_is_selected
-        /// </summary>
-        /// <returns>The <see cref="string"/></returns>
+     
         public string Which_IP_is_selected()
         {
             string option = "";
@@ -75,29 +68,15 @@ namespace SausageChatClient
             return option;
         }
 
-        /// <summary>
-        /// The ListBox_SelectionChanged
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="SelectionChangedEventArgs"/></param>
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Chat_Box_TextChanged
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="TextChangedEventArgs"/></param>
         private void Chat_Box_TextChanged(object sender, TextChangedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Send_message_Button_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+        
         private void Send_message_Button_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrEmpty(User_Message_client_Copy.Text))
@@ -111,38 +90,20 @@ namespace SausageChatClient
             }
         }
 
-        /// <summary>
-        /// The User_Message_client_TextChanged
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="TextChangedEventArgs"/></param>
+   
         private void User_Message_client_TextChanged(object sender, TextChangedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The ListBoxItem_Selected
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
         private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Select_IP_Combo_box_SelectionChanged
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="SelectionChangedEventArgs"/></param>
         private void Select_IP_Combo_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Button_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+       
         private void Button_Click_change_name(object sender, RoutedEventArgs e)
         {
             try
@@ -161,75 +122,41 @@ namespace SausageChatClient
                 SausageClient.Rename(Window1.UserInput);
         }
 
-        /// <summary>
-        /// The Ban_Button_client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+    
         private void Ban_Button_client_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Mute_Button_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+     
         private void Mute_Button_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Kick_Button_client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
         private void Kick_Button_client_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Info_button_client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
         private void Info_button_client_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Freinds_button_client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+       
         private void Freinds_button_client_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The Connect_Button_Client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+      
         private void Connect_Button_Client_Click(object sender, RoutedEventArgs e)
         {
             SausageClient.Start(Which_IP_is_selected());
         }
 
-        /// <summary>
-        /// The Add_Freind_button_client_Click
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="RoutedEventArgs"/></param>
+       
         private void Add_Freind_button_client_Click(object sender, RoutedEventArgs e)
         {
         }
 
-        /// <summary>
-        /// The User_Message_client_Copy_KeyDown
-        /// </summary>
-        /// <param name="sender">The sender<see cref="object"/></param>
-        /// <param name="e">The e<see cref="KeyEventArgs"/></param>
+       
         private void User_Message_client_Copy_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
