c202f389d672657ddc0d3514e9c1c7353b1257b9
ArchyInUse <archyinuse@gmail.com>
2019-08-14 02:41:06 +0300

    wrapped Kick Mute & Ban with exceptions (bad practice but will change later on)

--- SausageChatClient/Client_view_window.xaml.cs
+++ SausageChatClient/Client_view_window.xaml.cs
@@ -21,191 +21,216 @@ using System.Windows.Shapes;
 
 namespace SausageChatClient
 {
- 
-  public partial class MainWindow
-  {
-    
-    public MainWindow()
+
+    public partial class MainWindow
     {
-      SausageUserList.UiCtx = SynchronizationContext.Current;
-      ViewModel Vm = new ViewModel();
-      SausageClient.Mw = this;
-      SausageClient.Vm = Vm;
-      this.DataContext = Vm;
-    }
 
-
-
-    public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-    {
-      // Always test MultiValueConverter inputs for non-null
-      // (to avoid crash bugs for views in the designer)
-      if (values[0] is bool && values[1] is bool)
-      {
-        bool hasText = !(bool)values[0];
-        bool hasFocus = (bool)values[1];
-
-        if (hasFocus || hasText)
-          return Visibility.Collapsed;
-      }
-
-      return Visibility.Visible;
-    }
-
-    
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
-    {
-      throw new NotImplementedException();
-    }
-
-    
-    public string Which_IP_is_selected()
-    {
-      string option = "";
-      if (Select_IP_Combo_box.SelectedIndex == 0)
-        option = "Disco";
-      else if (Select_IP_Combo_box.SelectedIndex == 1)
-        option = "Monte";
-
-      return option;
-    }
-
-    
-    private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-    }
-
-   
-    private void Chat_Box_TextChanged(object sender, TextChangedEventArgs e)
-    {
-    }
-
-   
-    private void Send_message_Button_Click(object sender, RoutedEventArgs e)
-    {
-      if (!string.IsNullOrEmpty(User_Message_client_Copy.Text))
-      {
-        PacketFormat packet = new PacketFormat(PacketOption.ClientMessage)
+        public MainWindow()
         {
-          Guid = SausageClient.ClientInfo.Guid,
-          Content = User_Message_client_Copy.Text
-        };
-        SausageClient.Send(packet);
-      }
-    }
+            SausageUserList.UiCtx = SynchronizationContext.Current;
+            ViewModel Vm = new ViewModel();
+            SausageClient.Mw = this;
+            SausageClient.Vm = Vm;
+            this.DataContext = Vm;
+        }
 
-    private void User_Message_client_TextChanged(object sender, TextChangedEventArgs e)
-    {
-    }
 
-  
-    private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
-    {
-    }
 
-  
-    private void Select_IP_Combo_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-    }
-
-    
-    private void Button_Click_change_name(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        if (!SausageClient.Socket.Connected)
-          return;
-      }
-      catch (NullReferenceException)
-      {
-        return;
-      }
-      Window1 win2 = new Window1();
-      win2.ShowDialog();
-
-      if (Window1.UserInput != SausageClient.ClientInfo.Name)
-        SausageClient.Rename(Window1.UserInput);
-    }
-
-   
-    private void Ban_Button_client_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-  
-    private void Mute_Button_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-  
-    private void Kick_Button_client_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-    
-    private void Info_button_client_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-   
-    private void Freinds_button_client_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-   
-    private void Connect_Button_Client_Click(object sender, RoutedEventArgs e)
-    {
-      SausageClient.Start(Which_IP_is_selected());
-    }
-
-  
-    private void Add_Freind_button_client_Click(object sender, RoutedEventArgs e)
-    {
-    }
-
-    
-    private void User_Message_client_Copy_KeyDown(object sender, KeyEventArgs e)
-    {
-      if (e.Key == Key.Enter)
-      {
-        // send the message
-        PacketFormat packet = new PacketFormat(PacketOption.ClientMessage)
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-          Guid = SausageClient.ClientInfo.Guid,
-          Content = User_Message_client_Copy.Text
-        };
-        SausageClient.Send(packet);
-        // reset the text box
-        User_Message_client_Copy.Text = string.Empty;
-      }
+            // Always test MultiValueConverter inputs for non-null
+            // (to avoid crash bugs for views in the designer)
+            if (values[0] is bool && values[1] is bool)
+            {
+                bool hasText = !(bool)values[0];
+                bool hasFocus = (bool)values[1];
+
+                if (hasFocus || hasText)
+                    return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string Which_IP_is_selected()
+        {
+            string option = "";
+            if (Select_IP_Combo_box.SelectedIndex == 0)
+                option = "Disco";
+            else if (Select_IP_Combo_box.SelectedIndex == 1)
+                option = "Monte";
+
+            return option;
+        }
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+
+        private void Chat_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+
+        private void Send_message_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(User_Message_client_Copy.Text))
+            {
+                PacketFormat packet = new PacketFormat(PacketOption.ClientMessage)
+                {
+                    Guid = SausageClient.ClientInfo.Guid,
+                    Content = User_Message_client_Copy.Text
+                };
+                SausageClient.Send(packet);
+            }
+        }
+
+        private void User_Message_client_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Select_IP_Combo_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+
+        private void Button_Click_change_name(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!SausageClient.Socket.Connected)
+                    return;
+            }
+            catch (NullReferenceException)
+            {
+                return;
+            }
+            Window1 win2 = new Window1();
+            win2.ShowDialog();
+
+            if (Window1.UserInput != SausageClient.ClientInfo.Name)
+                SausageClient.Rename(Window1.UserInput);
+        }
+
+
+        private void Ban_Button_client_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Mute_Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Kick_Button_client_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Info_button_client_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Freinds_button_client_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Connect_Button_Client_Click(object sender, RoutedEventArgs e)
+        {
+            SausageClient.Start(Which_IP_is_selected());
+        }
+
+
+        private void Add_Freind_button_client_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void User_Message_client_Copy_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                // send the message
+                PacketFormat packet = new PacketFormat(PacketOption.ClientMessage)
+                {
+                    Guid = SausageClient.ClientInfo.Guid,
+                    Content = User_Message_client_Copy.Text
+                };
+                SausageClient.Send(packet);
+                // reset the text box
+                User_Message_client_Copy.Text = string.Empty;
+            }
+        }
+
+
+        private void Disconnect_Button_Client_Click(object sender, RoutedEventArgs e) => SausageClient.Disconnect();
+
+
+        private void ContextMenuBanUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SausageClient.Ban(SausageClient.Vm.SelectedUser.Guid);
+            }
+            catch (Exception) { }
+        }
+
+
+        private void ContextMenuKickUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SausageClient.Kick(SausageClient.Vm.SelectedUser.Guid);
+            }
+            catch (Exception) { }
+        }
+
+
+        private void ContextMenuMuteUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SausageClient.Mute(SausageClient.Vm.SelectedUser.Guid);
+            }
+            catch (Exception) { }
+        }
+
+
+        private void ContextMenuAddFriend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SausageClient.AddFriend(SausageClient.Vm.SelectedUser.Guid);
+            }
+            catch (Exception) { }
+        }
+
+
+        private void SendMessageToFriendMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void RemoveFriendMenuitem_Click(object sender, RoutedEventArgs e)
+        {
+        }
     }
-
-    
-    private void Disconnect_Button_Client_Click(object sender, RoutedEventArgs e) => SausageClient.Disconnect();
-
-  
-    private void ContextMenuBanUser_Click(object sender, RoutedEventArgs e) => SausageClient.Ban(SausageClient.Vm.SelectedUser.Guid);
-
-   
-    private void ContextMenuKickUser_Click(object sender, RoutedEventArgs e) => SausageClient.Kick(SausageClient.Vm.SelectedUser.Guid);
-
-  
-    private void ContextMenuMuteUser_Click(object sender, RoutedEventArgs e) => SausageClient.Mute(SausageClient.Vm.SelectedUser.Guid);
-
-   
-    private void ContextMenuAddFriend_Click(object sender, RoutedEventArgs e) => SausageClient.AddFriend(SausageClient.Vm.SelectedUser.Guid);
-
-  
-    private void SendMessageToFriendMenuItem_Click(object sender, RoutedEventArgs e)
-
-    {
-      DmWindow DMwindow = new DmWindow();
-      DMwindow.ShowDialog();
-    }
-
-   
-    private void RemoveFriendMenuitem_Click(object sender, RoutedEventArgs e)
-    {
-    }
-  }
 }
